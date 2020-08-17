ffd24b0190bdc493529caacd0958749dfda66c7f
Alphonso Tran <alphonsotran@gmail.com>
2020-08-18 01:44:40 +0200

    Add routes to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,48 @@
 import React from 'react';
-import CarSelection from './components/CarSelection';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import CarMakes from './pages/CarMakes';
 import ErrorBoundary from './components/ErrorBoundary';
+import CarModels from './pages/CarModels';
+import FuelTypes from './pages/FuelTypes';
+import BodyTypes from './pages/BodyTypes';
+import EngineCapacity from './pages/EngineCapacity';
+import Vehicles from './pages/Vehicles';
+import VehicleDetail from './pages/VehicleDetail';
+
+import { Title } from './styles';
 
 const App: React.FC = () => {
   return (
-    <ErrorBoundary>
-      <div>
-        <CarSelection />
-      </div>
-    </ErrorBoundary>
+    <>
+      <Title>Where's My Car?</Title>
+      <Router>
+        <ErrorBoundary>
+          <Switch>
+            <Route exact path="/">
+              <CarMakes />
+            </Route>
+            <Route exact path="/models">
+              <CarModels />
+            </Route>
+            <Route exact path="/fueltypes">
+              <FuelTypes />
+            </Route>
+            <Route exact path="/bodytypes">
+              <BodyTypes />
+            </Route>
+            <Route exact path="/enginecapacity">
+              <EngineCapacity />
+            </Route>
+            <Route exact path="/vehicles">
+              <Vehicles />
+            </Route>
+            <Route exact path="/vehicle">
+              <VehicleDetail />
+            </Route>
+          </Switch>
+        </ErrorBoundary>
+      </Router>
+    </>
   );
 };
 
