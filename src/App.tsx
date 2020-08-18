ffe8a5cb8be4fae1b9f10b0f05a6c8f1eaa6e23a
Alphonso Tran <alphonsotran@gmail.com>
2020-08-18 06:58:26 +0200

    Add functionality to switch themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CarMakes from './pages/CarMakes';
 import ErrorBoundary from './components/ErrorBoundary';
@@ -8,20 +9,41 @@ import BodyTypes from './pages/BodyTypes';
 import EngineCapacity from './pages/EngineCapacity';
 import Vehicles from './pages/Vehicles';
 import VehicleDetail from './pages/VehicleDetail';
+import Button from './components/Button';
+import lightTheme from './themes/light';
+import darkTheme from './themes/dark';
 
 import { Title, TitleWrapper } from './styles';
 
 const App: React.FC = () => {
+  const stored = localStorage.getItem('isLight');
+  const [isLight, setLightMode] = useState(stored === 'true' ? true : false);
+
   return (
-    <>
+    <ThemeProvider theme={isLight ? lightTheme : darkTheme}>
       <TitleWrapper>
         <Title>
           Where's My Car?{' '}
-          <span role="img" aria-label="car emoji">
+          <span
+            onClick={() => {
+              setLightMode(!isLight);
+              localStorage.setItem('isLight', `${!isLight}`);
+            }}
+            role="img"
+            aria-label="car emoji">
             🚘
           </span>
         </Title>
+        <Button
+          type="button"
+          label="Dark/Night"
+          onClick={() => {
+            setLightMode(!isLight);
+            localStorage.setItem('isLight', `${!isLight}`);
+          }}
+        />
       </TitleWrapper>
+
       <Router>
         <ErrorBoundary>
           <Switch>
@@ -49,7 +71,7 @@ const App: React.FC = () => {
           </Switch>
         </ErrorBoundary>
       </Router>
-    </>
+    </ThemeProvider>
   );
 };
 
